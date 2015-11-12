df663bbd2ab0da89bd64caefd76a1da280fc5ccb
Sean Flanigan <sflaniga@redhat.com>
2015-11-12 22:09:29 +1000

    Ensure that the JSON model is working

--- modules/hello/1.0.0/hello-1.0.0.js
+++ modules/hello/1.0.0/hello-1.0.0.js
@@ -2,18 +2,20 @@
 
 var _CTM$;function $CCMM$(){if (_CTM$===undefined)_CTM$=require('hello/1.0.0/hello-1.0.0-model').$CCMM$;return _CTM$;}
 ex$.$CCMM$=$CCMM$;
-var m$2=require('ceylon/language/1.2.0/ceylon.language-1.2.0');
-m$2.$addmod$(m$2,'ceylon.language/1.2.0');
-m$2.$addmod$(ex$,'hello/1.0.0');
+var m$c=require('ceylon/language/1.2.0/ceylon.language-1.2.0');
+m$c.$addmod$(m$c,'ceylon.language/1.2.0');
+m$c.$addmod$(ex$,'hello/1.0.0');
 ex$.$mod$ans$=[];
-ex$.$pkg$ans$hello=function(){return[m$2.shared()];};
+ex$.$pkg$ans$hello=function(){return[m$c.shared()];};
 
-//MethodDef run at run.ceylon (1:0-3:0)
+//MethodDef run at run.ceylon (1:0-5:0)
 function run(){
-m$2.print("Hello from Ceylon running in Nashorn jjs");
+m$c.print("Hello from Ceylon running in Nashorn jjs");
+var $d=m$c.$init$AppliedInterface$jsint()(m$c.List,{Type$Interface:{t:m$c.List,a:{Element$List:{t:m$c.$_Object}}}},undefined,{Element$List:{t:m$c.$_Object}});
+m$c.print($d);
 }
 ex$.run=run;
-run.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$2.Anything},ps:[],pa:1,d:['hello','run']};};
+run.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$c.Anything},ps:[],pa:1,d:['hello','run']};};
 });
 }(typeof define==='function' && define.amd ? define : function (factory) {
 if (typeof exports!=='undefined') { factory(require, exports, module);
--- source/hello/run.ceylon
+++ source/hello/run.ceylon
@@ -1,3 +1,5 @@
 shared void run() {
-    print("Hello from Ceylon running in Nashorn jjs");
+	print("Hello from Ceylon running in Nashorn jjs");
+	value listOfObjectType = `List<Object>`;
+	print(listOfObjectType);
 }
